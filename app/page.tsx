3bd7baa261c340581933ac6f1cfaa8147e4b701f
Suhel <suhel7822@gmail.com>
2024-02-24 01:52:41 +0530

    solve top background position error

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Section from "@/components/section/Section";
 import Link from "next/link";
--- components/headerbackground/HeaderBackground.tsx
+++ components/headerbackground/HeaderBackground.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
 const HeaderBackground = () => {
   return (
-    <div className="fixed left-[50%] top-[-100px] z-0 flex h-[35rem]  w-full translate-x-[-50%] items-center justify-center md:w-[64rem]">
-      <Image src="topGradient.svg" alt="top-gradient" fill />
+    <div className="fixed left-[50%] top-[-60%] z-0 flex h-[35rem] w-[1200px]  translate-x-[-50%] items-center justify-center md:top-[-40%]">
+      <div className="relative">
+        <div className="header-bg-elipse-1 absolute left-0 top-0"></div>
+        <div className="header-bg-elipse-2 absolute left-0 top-0 translate-x-[-50%] "></div>
+        <div className="header-bg-elipse-3 absolute right-0 top-0"></div>
+      </div>
     </div>
   );
 };
