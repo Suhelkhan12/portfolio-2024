6a60c8506f39b0ccc88ff91f9f7ae96d2797e42c
suhel khan <suhel7822@gmail.com>
2024-02-25 00:43:52 +0530

    changed hero text

--- app/page.tsx
+++ app/page.tsx
@@ -8,26 +8,28 @@ import ScrollDown from "@/components/scrolldown/ScrollDown";
 const page = () => {
   return (
     <Section>
-      <div className=" flex flex-col gap-8 text-basicDarkGray sm:gap-10">
-        <div className="flex flex-col gap-4 lg:gap-8">
-          <h1 className="text-[3rem] font-bold leading-normal md:text-[3.5rem] lg:text-[5rem]">
-            Hi👋, I'm <span className=" text-basicOnyx">Suhel</span>
-          </h1>
-          <div className=" w-full lg:w-[1000px]">
-            <p className=" text-[1rem] leading-normal md:text-[1.5rem] lg:text-[2rem]">
-              I'm a <span className=" text-basicBlack">UI engineer</span>. I
-              translate design concepts into functional code, bringing ideas to
-              life with precision and efficiency. I'm fueled by new tech,
-              football and anime.
-            </p>
+      <div className=" flex h-[80vh] flex-col justify-between">
+        <div className=" flex flex-col gap-8 text-basicDarkGray sm:gap-10">
+          <div className="flex flex-col gap-4 lg:gap-8">
+            <h1 className="text-[3rem] font-bold leading-normal md:text-[3.5rem] lg:text-[5rem]">
+              Hi👋, I'm <span className=" text-basicOnyx">Suhel</span>
+            </h1>
+            <div className=" w-full lg:w-[1000px]">
+              <p className=" text-[1.5rem] leading-normal md:text-[1.5rem] lg:text-[2rem]">
+                I'm a <span className=" text-basicBlack">UI engineer</span>. I
+                translate design concepts into functional code, bringing ideas
+                to life with precision and efficiency. I'm fueled by new tech,
+                football and anime.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 sm:flex-row ">
+            <PrimaryButton>See my resume</PrimaryButton>
+            <SecondaryButton>Let's connect</SecondaryButton>
           </div>
         </div>
-        <div className="flex flex-col gap-4 sm:flex-row ">
-          <PrimaryButton>See my resume</PrimaryButton>
-          <SecondaryButton>Let's connect</SecondaryButton>
-        </div>
+        <ScrollDown />
       </div>
-      <ScrollDown />
     </Section>
   );
 };
