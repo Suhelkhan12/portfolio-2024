7c955ad4ae614586c4f2cc3e9cbddfa80932c4a3
Suhel <suhel7822@gmail.com>
2024-02-26 19:15:56 +0530

    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section className="mx-auto pt-20 sm:max-w-[80rem] sm:pt-36 md:h-screen">
+      page
+    </section>
+  );
 };
 
 export default page;
--- components/headerbackground/HeaderBackground.tsx
+++ components/headerbackground/HeaderBackground.tsx
@@ -1,6 +1,6 @@
 const HeaderBackground = () => {
   return (
-    <div className="absolute left-[50%] top-[-15%] z-0 flex h-[35rem] w-[1200px] translate-x-[-50%]  items-center justify-center sm:top-[] md:top-[-10%]">
+    <div className="absolute left-[50%] top-[-15%] z-0 flex h-[35rem] w-[1200px] translate-x-[-50%]  items-center justify-center sm:top-[-30%] md:top-[-10%]">
       <div className="relative">
         <div className="header-bg-elipse-1 absolute left-0 top-0"></div>
         <div className="header-bg-elipse-2 absolute left-0 top-0 translate-x-[-50%] "></div>
