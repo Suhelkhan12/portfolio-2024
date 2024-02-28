08348c6ba4941ca31fdfc89567aa8f3cb0530a46
suhel khan <suhel7822@gmail.com>
2024-02-28 22:30:51 +0530

    make techstack page reponsive

--- app/techstack/page.tsx
+++ app/techstack/page.tsx
@@ -37,20 +37,20 @@ const page = () => {
   return (
     <section className="relative z-50 pt-20 sm:pt-36">
       <Wrapper>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 px-4">
           <H1>Techstack</H1>
-          <p className=" text-[1.5rem] font-normal text-basicMediumGray">
+          <p className="text-[1.25rem] font-normal text-basicMediumGray md:text-[1.5rem]">
             The dev tools I know and am currently learning.
           </p>
         </div>
       </Wrapper>
       <Separator />
       <Wrapper>
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4 px-4 md:gap-8">
           <H2 classnames="text-[3rem] font-bold text-basicDarkGray">
             Dev & Design
           </H2>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             {techCards
               .filter((card) => card.category === "development")
               .map((card) => (
@@ -61,11 +61,11 @@ const page = () => {
       </Wrapper>
       <Separator />
       <Wrapper>
-        <div className="flex flex-col gap-8">
-          <H2 classnames="text-[3rem] font-bold text-basicDarkGray">
+        <div className="flex flex-col gap-4 px-4 md:gap-8">
+          <H2 classnames=" text-[2rem] md:text-[3rem] font-bold text-basicDarkGray">
             Low-code & Freelancing
           </H2>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             {techCards
               .filter((card) => card.category === "low-code")
               .map((card) => (
@@ -76,11 +76,11 @@ const page = () => {
       </Wrapper>
       <Separator />
       <Wrapper>
-        <div className="flex flex-col gap-8">
-          <H2 classnames="text-[3rem] font-bold text-basicDarkGray">
+        <div className="flex flex-col gap-4 px-4 md:gap-8">
+          <H2 classnames=" text-[2rem] md:text-[3rem] font-bold text-basicDarkGray">
             Learning
           </H2>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             {techCards
               .filter((card) => card.category === "learning")
               .map((card) => (
--- components/techCard/TechCard.tsx
+++ components/techCard/TechCard.tsx
@@ -3,18 +3,27 @@ import Image from "next/image";
 
 const TechCard = (props: TechCardProps) => {
   return (
-    <div className="flex flex-col justify-between gap-10  rounded-lg border border-dardCardBorder bg-basicOnyx px-6 pb-6 pt-[3rem]">
+    <div className="flex flex-row gap-4 rounded-lg border border-dardCardBorder  bg-basicOnyx p-4 sm:flex-col sm:justify-between sm:gap-10 sm:pt-[3rem] md:px-6 md:pb-6">
       <Image
         src={props.image}
         alt={props.name}
         width={114}
         height={114}
-        className="mx-auto"
+        className="mx-auto hidden sm:block"
       />
-      <div className="flex items-center justify-between">
-        <p className="text-[1.25rem] font-semibold text-white">{props.name}</p>
+      <Image
+        src={props.image}
+        alt={props.name}
+        width={60}
+        height={60}
+        className="block sm:hidden"
+      />
+      <div className="flex grow items-center justify-between sm:grow-0 ">
+        <p className="text-base font-semibold text-white sm:text-[1.25rem]">
+          {props.name}
+        </p>
         <div className=" flex items-center justify-center rounded-full border border-[#272525] bg-transparent px-3 py-[6px]">
-          <p className=" text-[.875rem] font-normal text-basicDarkGray">
+          <p className=" text-[10px] font-normal text-basicDarkGray sm:text-[.875rem]">
             {props.tag}
           </p>
         </div>
