437c7aa49e3ebb03943c84422e19998377726fd4
suhel khan <suhel7822@gmail.com>
2024-02-29 09:53:04 +0530

    add global hero background

--- components/headerbackground/HeaderBackground.tsx
+++ components/headerbackground/HeaderBackground.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 
 const HeaderBackground = () => {
-  const path = usePathname();
+  // const path = usePathname();
   return (
     <div
-      className={`absolute left-[50%] top-[-15%] z-0 flex h-[35rem] w-[1200px] translate-x-[-50%]  items-center justify-center sm:top-[-30%] md:top-[-10%] ${path !== "/" && "hidden"}`}
+      className={`absolute left-[50%] top-[-15%] z-0 flex h-[35rem] w-[1200px] translate-x-[-50%]  items-center justify-center sm:top-[-30%] md:top-[-10%] `}
     >
       <div className="relative">
         <div className="header-bg-elipse-1 absolute left-0 top-0"></div>
