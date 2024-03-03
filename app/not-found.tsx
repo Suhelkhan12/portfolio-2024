9d3ae7d24af376d108fb407b22fd69f260b0a3a2
suhel khan <suhel7822@gmail.com>
2024-03-03 21:14:50 +0530

    make 404 page responsive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
 import Link from "next/link";
 import PrimaryButton from "@/components/buttons/PrimaryButton";
+import Wrapper from "@/components/wrapper/Wrapper";
 
 export default function NotFound() {
   return (
-    <section className="relative z-50 flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-8xl font-bold text-ligthFaintWhite">404</h1>
-        <p className="text-2xl text-basicLightGray">
-          The page you are looking for doesn't exist or has been moved.
-        </p>
-        <Link href="/">
-          <PrimaryButton>Return to Home</PrimaryButton>
-        </Link>
-      </div>
+    <section className="relative z-50 flex h-[70vh] items-center justify-center">
+      <Wrapper>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-8xl font-bold text-ligthFaintWhite">404</h1>
+          <p className="text-center text-2xl text-basicLightGray">
+            The page you are looking for doesn't exist or has been moved.
+          </p>
+          <Link href="/">
+            <PrimaryButton>Return to Home</PrimaryButton>
+          </Link>
+        </div>
+      </Wrapper>
     </section>
   );
 }
