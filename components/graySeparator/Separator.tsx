062c1f0fc5c872a58ff079beb293ef5304f648e2
suhel khan <suhel7822@gmail.com>
2024-03-04 20:06:18 +0530

    change separator margin for mobile

--- components/graySeparator/Separator.tsx
+++ components/graySeparator/Separator.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Separator = () => {
-  return <div className="my-16 h-[1px] w-full bg-white/10"></div>;
+  return (
+    <div className="my-8 h-[1px] w-full bg-white/10  sm:my-10 md:my-12 lg:my-16"></div>
+  );
 };
 
 export default Separator;
