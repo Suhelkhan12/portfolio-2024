3e3973b5de6e18fd074fd47e6167ae66805948a6
suhel khan <suhel7822@gmail.com>
2024-03-05 19:13:03 +0530

    finished clickText functionallity

--- components/portrait/ClickText.tsx
+++ components/portrait/ClickText.tsx
@@ -4,11 +4,21 @@ import { useState } from "react";
 
 const ClickText = () => {
   const [currText, setCurrText] = useState("Click this text.");
-  const handleClick = () => {};
+  const [currIndex, setCurrIndex] = useState(0);
+
+  const handleClick = () => {
+    // Increment the index, looping back to 0 if it exceeds the array length
+    setCurrIndex((prevIndex) => (prevIndex + 1) % portraitText.length);
+    setCurrText(portraitText[currIndex]);
+  };
+
   return (
-    <div className=" clicktext absolute -top-20 left-[50%] translate-x-[-50%] cursor-pointer rounded-full bg-basicLightGray px-5 py-4 text-black">
-      <button onClick={handleClick}>{currText}</button>
-    </div>
+    <button
+      className="clicktext absolute -top-20 right-0 hidden cursor-pointer rounded-full bg-basicLightGray px-5 py-4 text-black sm:block"
+      onClick={handleClick}
+    >
+      {currText}
+    </button>
   );
 };
 
