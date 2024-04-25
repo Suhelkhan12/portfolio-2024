30ed16e5cc6cd20899606cbd25de892436d91526
suhelunparallel <suhel@parallelhq.com>
2024-04-25 16:41:28 +0530

    inor bug solved

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -89,7 +89,7 @@ const page = () => {
                 </div>
               </div>
             </div>
-            <div className="my-6 flex flex-col gap-2 text-[1.5rem]  font-medium text-ligthFaintWhite md:my-8">
+            <div className="my-6 flex flex-col gap-2 text-base  font-medium  text-ligthFaintWhite md:my-8 md:text-[1.5rem]">
               <p className="">
                 Alternatively, here&apos;s a fancy sheet of paper.
               </p>
