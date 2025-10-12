a17af59e16fefae965954911d1db7aa22b4374d8
Suhel <suhel7822@gmail.com>
2025-10-13 00:19:10 +0530

    resume error fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -76,7 +76,7 @@ const page = () => {
             Alternatively, here&apos;s a fancy sheet of paper
           </span>
           <Link
-            href={"/suhel_resume_2025.pdf"}
+            href={"/public/suhel-resume.pdf"}
             target="_blank"
             className="flex items-center justify-center  text-blue-500 transition-colors hover:text-blue-400"
           >
